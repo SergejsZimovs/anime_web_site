04d984ac5c3f7eba4e3543ab3153cb2ea2389a2a
sergejs.zimovs <sergejs.zimovs97@gmail.com>
2025-02-24 21:33:53 +0200

    cycles

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+let num = 50;
+
+// while (num <= 66) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num <= 66);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
