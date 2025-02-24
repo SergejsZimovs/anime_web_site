e4600f1657bd1f2ed0638a4790455fb13f58309f
sergejs.zimovs <sergejs.zimovs97@gmail.com>
2025-02-24 21:46:51 +0200

    Elo4ka

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,11 @@
-let num = 50;
+let result = "";
+const length = 100;
 
-// while (num <= 66) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// } while (num <= 66);
-
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*";
     }
-    console.log(i);
+    result += "\n";
 }
+
+console.log(result);
