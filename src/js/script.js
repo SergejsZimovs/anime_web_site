784770488b35d15287e369e17fa380c3ad12196e
sergejs.zimovs <sergejs.zimovs97@gmail.com>
2025-02-26 15:24:48 +0200

    home_work

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,70 @@ for (let i = 1; i < length; i++) {
 }
 
 console.log(result);
+
+function firstTask() {
+    // Пишем решение вот тут
+    for (let i = 5; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+firstTask();
+
+function secondTask() {
+    // Пишем решение вот тут
+    for (let i = 20; i >= 10; i--) {
+        if (i === 13) {
+            break;
+        }
+        console.log(i);
+    }
+}
+secondTask();
+
+function thirdTask() {
+    for (let i = 2; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+thirdTask();
+
+// function fourthTask() {
+//     for (let i = 2; i <= 16; i++) {
+//         if (i % 2 === 0) {
+//             continue;
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+// fourthTask();
+
+function fourthTask() {
+    let i = 2;
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+            i++;
+        }
+    }
+}
+fourthTask();
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+    // Пишем решение вот тут
+    for (let i = 5; i <= 10; i++) {
+        arrayOfNumbers.push(i);
+    }
+    console.log(arrayOfNumbers);
+    // Не трогаем
+    return arrayOfNumbers;
+}
+fifthTask();
