c26920353721a7f82f16ced08a21d4ec85c44e3e
sergejs.zimovs <sergejs.zimovs97@gmail.com>
2025-02-26 18:02:35 +0200

    home_work2.1

--- src/js/script.js
+++ src/js/script.js
@@ -1,78 +1,49 @@
-let result = "";
-const length = 100;
-
-for (let i = 1; i < length; i++) {
-    for (let j = 0; j < i; j++) {
-        result += "*";
-    }
-    result += "\n";
-}
-
-console.log(result);
-
 function firstTask() {
-    // Пишем решение вот тут
-    for (let i = 5; i <= 10; i++) {
-        console.log(i);
-    }
-}
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
 
+    // Пишем решение вот тут
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i]);
+    }
+    console.log(result);
+    // Не трогаем
+    return result;
+}
 firstTask();
 
 function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, "Shopping", 20, "Homework"];
+
     // Пишем решение вот тут
-    for (let i = 20; i >= 10; i--) {
-        if (i === 13) {
-            break;
+    for (let i = 0; i < data.length; i++) {
+        if (typeof data[i] === "number") {
+            data[i] = data[i] * 2;
         }
-        console.log(i);
+        if (typeof data[i] === "string") {
+            data[i] = data[i] + " - done";
+        }
     }
+    console.log(data);
+    // Не трогаем
+    return data;
 }
 secondTask();
 
 function thirdTask() {
-    for (let i = 2; i <= 10; i++) {
-        if (i % 2 === 0) {
-            console.log(i);
-        }
-    }
-}
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, "Shopping", 20, "Homework"];
+    const result = [];
 
-thirdTask();
-
-// function fourthTask() {
-//     for (let i = 2; i <= 16; i++) {
-//         if (i % 2 === 0) {
-//             continue;
-//         } else {
-//             console.log(i);
-//         }
-//     }
-// }
-// fourthTask();
-
-function fourthTask() {
-    let i = 2;
-    while (i <= 16) {
-        if (i % 2 === 0) {
-            i++;
-            continue;
-        } else {
-            console.log(i);
-            i++;
-        }
-    }
-}
-fourthTask();
-
-function fifthTask() {
-    const arrayOfNumbers = [];
     // Пишем решение вот тут
-    for (let i = 5; i <= 10; i++) {
-        arrayOfNumbers.push(i);
+    for (let i = 0; i < data.length; i++) {
+        result.push(data[i]);
     }
-    console.log(arrayOfNumbers);
+    result.reverse();
+    console.log(result);
     // Не трогаем
-    return arrayOfNumbers;
+    return result;
 }
-fifthTask();
+thirdTask();
